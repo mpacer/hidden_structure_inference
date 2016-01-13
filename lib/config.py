9bbb9f1f0a1014344a517db010d5dc7e36518c5e
michaelpacer <michaelpacer@gmail.com>
2016-01-12 19:35:12 -0800

    changed to match master to test appropriately large numbers of parameters

--- lib/config.py
+++ lib/config.py
@@ -77,8 +77,8 @@ edge_semantics={
     }
 }
 
-param_sample_size = 2
-stigma_sample_size = 100
+param_sample_size = 30
+stigma_sample_size = 5
 # scale_free_bounds = (10**(-.1),10**(.1))
 # scale_free_bounds = (10**(-.25),10**(.25))
 # scale_free_bounds = (10**-1,10**1)
